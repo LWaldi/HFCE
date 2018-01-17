b0d63b62039361e2fb3d880659fb4e4bdb642684
LWaldi <34715526+LWaldi@users.noreply.github.com>
2018-01-17 14:27:28 +0100

    Add files via upload

--- this_python.py
+++ this_python.py
@@ -5,9 +5,10 @@ import random
 from pygame.locals import *
 from pygame.compat import unichr_, unicode_
 
-##Variables#
 
-# Colors
+##Variables##
+
+#Colors#
 col_white = (250, 250, 250)
 col_black = (0, 0, 0)
 col_grey = (220, 220, 220)
@@ -32,6 +33,9 @@ screen.fill(BACKGR_COL)
 font = pygame.font.Font(None, 80)
 font_small = pygame.font.Font(None, 40) 
 
+image = pygame.image.load ('cat.png')
+
+
 
 ##METHOD##
 
@@ -45,7 +49,7 @@ def main ():
         pygame.display.get_surface().fill(BACKGR_COL)
         
    #Transitions#
-       for event in pygame.event.get():                        
+    for event in pygame.event.get():                        
             if STATE == "Welcome":
                 if event.type == KEYDOWN and event.key == K_SPACE:
                     STATE = "Instruction"
@@ -58,14 +62,17 @@ def main ():
                     
             if STATE == "Fixation":
                 if event.type == KEYDOWN and event.key == K_SPACE:
+                    degrees_to_rotate = random.randint (0,360) 
                     STATE = "Pair of objects"
                     print(STATE)
                     
             elif STATE == "Pair of objects":
                 if event.type == KEYDOWN and event.key in KEYS.values():
+                    #rotation and mirroring images
+                    
                     time_when_reacted = time()
                     this_reaction_time = time_when_reacted - time_when_presented
-                    this_correctness = (event.key == KEYS[this_color]) #YES/NO
+                    this_correctness = (event.key == KEYS[image]) #YES/NO
                     STATE = "Feedback"
                     time_when_presented = time()
                     print(STATE)
@@ -75,55 +82,157 @@ def main ():
                     if trial_number < 20:
                         STATE = "Fixation"
                     else:
-                        STATE = "goodbye"
+                        STATE = "Goodbye"
                     print(STATE)    
             
             if event.type == QUIT:
-                STATE = "quit" 
+                STATE = "Quit" 
         
-        #Automatic Transitions#
+   #Automatic Transitions#
         
-        if STATE == "Fixation": 
+    if STATE == "Fixation": 
             if trial_number < 20:
                 if (time() - time_when_presented) > 1:
                     STATE = "Pair of objects"
                     print (STATE)
             else: 
-                STATE = "goodbye"
+                STATE = "Goodbye"
                 print (STATE)
         
         
-        elif STATE == "Fixation":
+    elif STATE == "Fixation":
             trial_number = trial_number + 1
-            this_word  = pick_color() #pick object#
-            this_color = pick_color() #pick object#
             time_when_presented = time()
             STATE = "Pair of objects"
             print(STATE)
         
         
-      # Drawing to the screen  Change to our States
-        if STATE == "welcome":
+     # Drawing to the screen#
+    if STATE == "Welcome":
             draw_welcome()
-            draw_button(SCREEN_SIZE[0]*1/5, 450, "Red: X", col_red)
-            draw_button(SCREEN_SIZE[0]*3/5, 450, "Green: N", col_green)
-            draw_button(SCREEN_SIZE[0]*4/5, 450, "Blue: M", col_blue)
+            draw_button(SCREEN_SIZE[0]*3/5, 450, "Press Space to continue", col_black)
+            
+            
+    if STATE == "Instruction":
+            draw_instruction()
+            
+    if STATE == "Fixation":
+            draw_fixation()
         
-        if STATE == "wait_for_response":
-            draw_stimulus(this_color, this_word)
-            draw_button(SCREEN_SIZE[0]*1/5, 450, "Red: X", col_red)
-            draw_button(SCREEN_SIZE[0]*3/5, 450, "Green: N", col_green)
-            draw_button(SCREEN_SIZE[0]*4/5, 450, "Blue: M", col_blue)
+    if STATE == "Pair of objects":
+            draw_image1 ()
+            draw_image2 ()
+            draw_button(SCREEN_SIZE[0]*1/5, 450, "Yes", col_green)
+            draw_button(SCREEN_SIZE[0]*4/5, 450, "No", col_red)
         
-        if STATE == "feedback":
+    if STATE == "Feedback":
             draw_feedback(this_correctness, this_reaction_time)
         
-        if STATE == "goodbye":
+    if STATE == "Goodbye":
             draw_goodbye()
         
-        if STATE == "quit":
+    if STATE == "Quit":
             pygame.quit()
             sys.exit()
 
-        pygame.display.update()  
+    pygame.display.update()  
+        
+        
+#Defining methods#
+        
+def draw_welcome ():
+    text_surface = font.render("Mental Rotation Task", True, col_black, BACKGR_COL)
+    text_rectangle = text_surface.get_rect()
+    text_rectangle.center = (SCREEN_SIZE[0]/2.0,150)
+    screen.blit(text_surface, text_rectangle)
+    text_surface = font_small.render("Press Spacebar to continue", True, col_black, BACKGR_COL)
+    text_rectangle = text_surface.get_rect()
+    text_rectangle.center = (SCREEN_SIZE[0]/2.0,300)
+    screen.blit(text_surface, text_rectangle)
+    
+def draw_instrcution (): #instructional text#
+    text_surface = font.render("Instruction", True, col_black, BACKGR_COL)
+    text_rectangle = text_surface.get_rect()
+    text_rectangle.center = (SCREEN_SIZE[0]/2.0,150)
+    screen.blit(text_surface, text_rectangle)
+    text_surface = font_small.render("Two objects will be presented on the screen. They can be rotated or mirrored. Please choose if these objects are the same by pressing Y for yes or N for no.", True, col_black, BACKGR_COL)
+    text_rectangle = text_surface.get_rect()
+    text_rectangle.center = (SCREEN_SIZE[0]/2.0,200)
+    screen.blit(text_surface, text_rectangle)
+    text_surface = font_small.render("Press Spacebar to start experiment", True, col_black, BACKGR_COL)
+    text_rectangle = text_surface.get_rect()
+    text_rectangle.center = (SCREEN_SIZE[0]/2.0,300)
+    screen.blit(text_surface, text_rectangle)
+    
+def draw_fixation(): #big cross in middle#
+    text_surface = font_small.render("X", True, col_red, BACKGR_COL)
+    text_rectangle = text_surface.get_rect()
+    text_rectangle.center = (SCREEN_SIZE[0]/2.0,250)
+    screen.blit(text_surface, text_rectangle)
+    
+def draw_button(xpos, ypos, label, color):
+    text = font_small.render(label, True, color, BACKGR_COL)
+    text_rectangle = text.get_rect()
+    text_rectangle.center = (xpos, ypos)
+    screen.blit(text, text_rectangle
+    
+def flip_image(image_path, saved_location):
+    image_obj = Image.open(image_path)
+    rotated_image = image_obj.transpose(Image.FLIP_LEFT_RIGHT)
+    rotated_image.save(saved_location)
+    rotated_image.show()
+    
+def rotate(image_path, degrees_to_rotate, saved_location):
+    image_obj = Image.open(image_path)
+    rotated_image = image_obj.rotate(degrees_to_rotate)
+    rotated_image.save(saved_location)
+    rotated_image.show()
+
+def draw_image1(): #2 pictures being rotated#
+    
+        
+    
+def draw_image2():
+    
+       
+def draw_feedback (this_correctness, this_reaction_time):
+     if correct:
+        text_surface = font_small.render("correct", True, col_black, BACKGR_COL)
+        text_rectangle = text_surface.get_rect()
+        text_rectangle.center = (SCREEN_SIZE[0]/2.0,150)
+        screen.blit(text_surface, text_rectangle)
+        text_surface = font_small.render(str(int(reaction_time * 1000)) + "ms", True, col_black, BACKGR_COL)
+        text_rectangle = text_surface.get_rect()
+        text_rectangle.center = (SCREEN_SIZE[0]/2.0,200)
+        screen.blit(text_surface, text_rectangle)
+    else:
+        text_surface = font_small.render("Wrong key!", True, col_red, BACKGR_COL)
+        text_rectangle = text_surface.get_rect()
+        text_rectangle.center = (SCREEN_SIZE[0]/2.0,150)
+        screen.blit(text_surface, text_rectangle)
+    text_surface = font_small.render("Press Spacebar to continue", True, col_black, BACKGR_COL)
+    text_rectangle = text_surface.get_rect()
+    text_rectangle.center = (SCREEN_SIZE[0]/2.0,300)
+    screen.blit(text_surface, text_rectangle)
+    
+    
+def draw_goodbye():
+    text_surface = font_small.render("END OF THE EXPERIMENT", True, col_black, BACKGR_COL)
+    text_rectangle = text_surface.get_rect()
+    text_rectangle.center = (SCREEN_SIZE[0]/2.0,150)
+    screen.blit(text_surface, text_rectangle)
+    text_surface = font_small.render("Thank you for your participation!", True, col_black, BACKGR_COL)
+    text_rectangle = text_surface.get_rect()
+    text_rectangle.center = (SCREEN_SIZE[0]/2.0,200)
+    screen.blit(text_surface, text_rectangle)
+    text_surface = font_small.render("Close the application.", True, col_black, BACKGR_COL)
+    text_rectangle = text_surface.get_rect()
+    text_rectangle.center = (SCREEN_SIZE[0]/2.0,250)
+    screen.blit(text_surface, text_rectangle)
+    
+
+        
+        
+        
+main()
         
